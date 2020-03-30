bab60d938ca43ff1fe37712314077e3969380722
Guido van Dijk <guido.van.dijk@thevalley.nl>
2020-03-30 20:25:32 +0200

    added firebase for tournament creation

--- src/views/tournament/Create.vue
+++ src/views/tournament/Create.vue
@@ -13,11 +13,11 @@
           <b-input v-model="tournament[0].name" placeholder="Tournament name.."></b-input>
         </b-field>
         <b-field label="Tournament Type">
-          <b-select v-model="tournament[0].selectedIndex" :placeholder="tournamentList[0]">
+          <b-select v-model="tournament[0].type" placeholder="Pick a tournament type">
             <option v-for="(data, index) in tournamentList" :value="index" :key="index">{{ data }}</option>
           </b-select>
         </b-field>
-        <b-field label="Number of teams">
+        <b-field label="Number of teams (min, max)">
           <b-slider
             v-model="tournament[0].numberOfTeams"
             :min="numberOfTeams.min"
@@ -26,7 +26,7 @@
             ticks
           ></b-slider>
         </b-field>
-        <b-field label="Number of players per team">
+        <b-field label="Number of players per team (min, max)">
           <b-slider
             v-model="tournament[0].numberOfPlayers"
             :min="numberOfPlayers.min"
@@ -44,23 +44,25 @@
           <b-datetimepicker
             placeholder="Date until people can sign up..."
             icon="calendar-today"
+            :datetime-formatter="this.dateFormatter"
+            :datetime-parser="this.dateFormatter"
             v-model="tournament[1].signupDate"
-            editable
           ></b-datetimepicker>
         </b-field>
         <b-field label="Tournament start date">
           <b-datetimepicker
             placeholder="Tournament starting date..."
+            :datetime-formatter="this.dateFormatter"
+            :datetime-parser="this.dateFormatter"
             icon="calendar-today"
             v-model="tournament[1].startDate"
-            editable
           ></b-datetimepicker>
         </b-field>
       </div>
     </b-step-item>
 
     <b-step-item label="Finish">
-      <div class="box">
+      <div class="box" v-if="activeStep == 2">
         <ul class>
           <li>
             Tournament Name:
@@ -76,11 +78,11 @@
           </li>
           <li>
             Sign-up end date:
-            <strong>{{tournament[1].signupDate}}</strong>
+            <strong>{{this.dateFormatter(tournament[1].signupDate)}}</strong>
           </li>
           <li>
             Tournament starting date:
-            <strong>{{tournament[1].startDate}}</strong>
+            <strong>{{this.dateFormatter(tournament[1].startDate)}}</strong>
           </li>
         </ul>
       </div>
@@ -88,26 +90,35 @@
 
     <template slot="navigation" slot-scope="{previous, next}">
       <div class="steps-navigation box has-background-transparent">
-        <b-button
-          type="is-grey-light"
-          icon-left="arrow-left"
-          :disabled="previous.disabled"
-          @click.prevent="previous.action"
-        >Previous</b-button>
-        <template v-if="!next.disabled">
+        <p class="has-text-danger">
+          <span v-show="showErrorMessage">Please fill in all the inputs</span>
+        </p>
+        <div class="buttons">
           <b-button
-            type="is-primary"
-            icon-right="arrow-right"
-            :disabled="false"
-            @click.prevent="validate(next)"
-          >Next</b-button>
-        </template>
-        <template v-else>
-          <b-button type="is-primary" icon-right="arrow-right" :disabled="false">Create</b-button>
-        </template>
+            type="is-grey-light"
+            icon-left="arrow-left"
+            :disabled="previous.disabled"
+            @click.prevent="previous.action"
+          >Previous</b-button>
+          <template v-if="!next.disabled">
+            <b-button
+              type="is-primary"
+              icon-right="arrow-right"
+              :disabled="false"
+              @click.prevent="validate(next)"
+            >Next</b-button>
+          </template>
+          <template v-else>
+            <b-button
+              type="is-primary"
+              icon-right="arrow-right"
+              :disabled="false"
+              @click="createTournament"
+            >Create</b-button>
+          </template>
+        </div>
       </div>
     </template>
-    <p class="has-text-danger" v-show="showErrorMessage">Please fill in all the inputs</p>
   </b-steps>
 </template>
 <script>
@@ -122,7 +133,7 @@ export default {
       tournament: [
         {
           name: null,
-          selectedIndex: 0,
+          type: null,
           numberOfTeams: [1, 18],
           numberOfPlayers: [1, 4]
         },
@@ -146,19 +157,31 @@ export default {
       }
     };
   },
-  mounted() {
-    // this.test();
-  },
-  methods: {
-    test() {
-      const teamsCollection = db.collection("teams");
-      teamsCollection.get().then(snapshot => {
-        snapshot.forEach(doc => {
-          const data = doc.data();
 
-          console.log(data);
-        });
+  methods: {
+    dateFormatter(dt) {
+      return dt.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit"
       });
+    },
+    createTournament() {
+      const td = Object.assign(this.tournament[0], this.tournament[1]);
+
+      db.collection("tournament")
+        .doc()
+        .set({
+          name: td.name,
+          type: td.type,
+          numberOfTeams: td.numberOfTeams,
+          numberOfPlayers: td.numberOfPlayers,
+          signupDate: td.signupDate,
+          startDate: td.startDate
+        });
     },
     validate(next) {
       const inputIsNull = Object.values(this.tournament[this.activeStep]).some(

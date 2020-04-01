d0f26f7b676234976db4c093a6a80289de55cf27
Guido van Dijk <guido.van.dijk@thevalley.nl>
2020-04-01 22:01:50 +0200

    details now shall full list of participants

--- src/components/activities/Details.vue
+++ src/components/activities/Details.vue
@@ -3,10 +3,21 @@
     <div class="columns is-multiline">
       <div class="column is-8">
         <div class="column-header is-space-between">
-          <p class="title is-4">Participants ({{ this.data.length }})</p>
-          <b-input v-model="searchPlayer" placeholder="Search for player..." icon="magnify"></b-input>
+          <p class="title is-4">
+            Participants ({{ this.participants.length }})
+          </p>
+          <b-input
+            v-model="searchPlayer"
+            placeholder="Search for player..."
+            icon="magnify"
+          ></b-input>
         </div>
-        <b-table :data="filterByTerm" :columns="columns" striped class></b-table>
+        <b-table
+          :data="filterByTerm"
+          :columns="columns"
+          striped
+          class
+        ></b-table>
       </div>
       <div class="column">
         <p class="title is-4">Tournament Details</p>
@@ -21,7 +32,8 @@
                   v-for="(data, index) in this.tournmamentType"
                   :value="data"
                   :key="index"
-                >{{ data }}</option>
+                  >{{ data }}</option
+                >
               </b-select>
             </b-field>
             <b-field label="Tournament sign-up end date">
@@ -59,7 +71,12 @@
 
             <div class="steps-navigation">
               <b-button type="is-primary">Save changes</b-button>
-              <b-button label="Reset" outlined inverted type="is-grey-light"></b-button>
+              <b-button
+                label="Reset"
+                outlined
+                inverted
+                type="is-grey-light"
+              ></b-button>
             </div>
           </form>
         </div>
@@ -69,103 +86,112 @@
 </template>
 
 <script>
-export default {
-  name: "Details",
-  // props: ["data", "teams"],
-  data() {
-    return {
-      maxTeamSize: 5,
-      numberOfTeams: {
-        value: [1, 18],
-        max: 30,
-        min: 3
+  export default {
+    name: "Details",
+    props: ["data", "teams"],
+    data() {
+      return {
+        maxTeamSize: 5,
+        numberOfTeams: {
+          value: [1, 18],
+          max: 30,
+          min: 3
+        },
+        numberOfPlayer: {
+          value: [1, 4],
+          max: 10,
+          min: 1
+        },
+        searchPlayer: "",
+        tournmamentType: [
+          "Single Elimination",
+          "Double Elimination",
+          "Round Robin"
+        ],
+        participants: [],
+        // data: [
+        //   {
+        //     league_name: "Jesse",
+        //     discord_name: "Simmons",
+        //     team_name: "Team 1",
+        //     team_size: 4
+        //   },
+        //   {
+        //     league_name: "John",
+        //     discord_name: "Jacobs",
+        //     team_name: "Team 2",
+        //     team_size: 5
+        //   },
+        //   {
+        //     league_name: "Tina",
+        //     discord_name: "Gilbert",
+        //     team_name: "Team 3",
+        //     team_size: 2
+        //   },
+        //   {
+        //     league_name: "Clarence",
+        //     discord_name: "Flores",
+        //     team_name: "Team 4",
+        //     team_size: 1
+        //   },
+        //   {
+        //     league_name: "Anne",
+        //     discord_name: "Lee",
+        //     team_name: "Team 5",
+        //     team_size: 2
+        //   }
+        // ],
+        columns: [
+          {
+            field: "name",
+            label: "Name",
+            sortable: true
+          },
+          {
+            field: "team",
+            label: "Team Name",
+            sortable: true
+          },
+          {
+            field: "teamSize",
+            label: "Team Size",
+            sortable: true
+          }
+        ]
+      };
+    },
+    computed: {
+      filterByTerm() {
+        return this.participants.filter(participant => {
+          return participant.name.toLowerCase().includes(this.searchPlayer);
+        });
+      }
+    },
+    mounted() {
+      this.getTeamSize();
+      this.createParticipantsList();
+    },
+    methods: {
+      createParticipantsList() {
+        const allParticipants = [];
+
+        this.teams.map(team => {
+          team.players.map(player => {
+            allParticipants.push({
+              name: player,
+              team: team.teamName,
+              teamSize: team.players.length
+            });
+          });
+        });
+        this.participants = allParticipants;
       },
-      numberOfPlayer: {
-        value: [1, 4],
-        max: 10,
-        min: 1
-      },
-      searchPlayer: "",
-      tournmamentType: [
-        "Single Elimination",
-        "Double Elimination",
-        "Round Robin"
-      ],
-      data: [
-        {
-          league_name: "Jesse",
-          discord_name: "Simmons",
-          team_name: "Team 1",
-          team_size: 4
-        },
-        {
-          league_name: "John",
-          discord_name: "Jacobs",
-          team_name: "Team 2",
-          team_size: 5
-        },
-        {
-          league_name: "Tina",
-          discord_name: "Gilbert",
-          team_name: "Team 3",
-          team_size: 2
-        },
-        {
-          league_name: "Clarence",
-          discord_name: "Flores",
-          team_name: "Team 4",
-          team_size: 1
-        },
-        {
-          league_name: "Anne",
-          discord_name: "Lee",
-          team_name: "Team 5",
-          team_size: 2
-        }
-      ],
-      columns: [
-        {
-          field: "league_name",
-          label: "League Name",
-          sortable: true
-        },
-        {
-          field: "discord_name",
-          label: "Discord Name",
-          sortable: true
-        },
-        {
-          field: "team_name",
-          label: "Team Name",
-          sortable: true
-        },
-        {
-          field: "team_size",
-          label: "Team Size",
-          sortable: true
-        }
-      ]
-    };
-  },
-  computed: {
-    filterByTerm() {
-      return this.data.filter(data => {
-        return (
-          data.league_name.toLowerCase().includes(this.searchPlayer) ||
-          data.discord_name.toLowerCase().includes(this.searchPlayer)
-        );
-      });
+      getTeamSize() {
+        // console.log(this.teams);
+        // this.data.forEach(data => {
+        //   data.team_size = `${data.team_size}/${this.maxTeamSize}`;
+        // });
+      }
     }
-  },
-  mounted() {
-    this.getTeamSize();
-  },
-  methods: {
-    getTeamSize() {
-      // this.data.forEach(data => {
-      //   data.team_size = `${data.team_size}/${this.maxTeamSize}`;
-      // });
-    }
-  }
-};
+  };
 </script>

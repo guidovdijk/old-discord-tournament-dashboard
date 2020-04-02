270743b6cf193ba189fc2b4d95727940f2211d8b
Guido van Dijk <guido.van.dijk@thevalley.nl>
2020-04-02 17:48:08 +0200

    Added styling to tournament list overview

--- src/views/tournament/ListOverview.vue
+++ src/views/tournament/ListOverview.vue
@@ -1,25 +1,47 @@
 <template>
   <div>
-    <div
-      class="box is-space-between is-flex is-flex-end"
-      v-for="(t, index) in tournaments"
-      :key="index"
-    >
-      <div>
-        <p class="title is-5">
-          <strong>{{t.name}}</strong>
-        </p>
-        <p class="subtitle is-6">{{t.type}}</p>
-        <p>
-          Start date: {{t.startDate}}
-          <br />
-          Sign up end date: {{t.signupDate}}
-        </p>
-      </div>
-      <div>
-        <b-button type="is-primary" @click="goToDetailPage(t.id)">play along</b-button>
-      </div>
-    </div>
+    <b-table :data="tournaments" hoverable class="has-border-spacing no-header-background">
+      <template slot-scope="props">
+        <b-table-column field="name" label="Tournament name">
+          {{
+          props.row.name
+          }}
+        </b-table-column>
+
+        <b-table-column field="type" label="Type">
+          {{
+          props.row.type
+          }}
+        </b-table-column>
+
+        <b-table-column field="startDate" label="Start Date">
+          {{
+          props.row.startDate
+          }}
+        </b-table-column>
+
+        <b-table-column field="status" label="Status" class="is-v-centered" centered>
+          <b-tooltip
+            :label="props.row.status ? 'Tournament is open' : 'Tournament is closed'"
+            position="is-right"
+            class="is-center has-tag"
+            animated
+            type="is-dark"
+          >
+            <b-tag :class="getTournamentStatusStyle(props.row.status)" class="is-circle" rounded></b-tag>
+          </b-tooltip>
+        </b-table-column>
+
+        <b-table-column field="startDate" label=" ">
+          <b-button
+            type="is-primary"
+            size="is-small"
+            :disabled="!props.row.status"
+            @click="goToDetailPage(props.row.id)"
+          >play along</b-button>
+        </b-table-column>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -36,46 +58,48 @@ export default {
   beforeMount() {
     this.getAllTournaments();
   },
+  computed: {},
   methods: {
+    getTournamentStatusStyle(status) {
+      return status ? "is-primary" : "is-danger";
+    },
     goToDetailPage(id) {
       this.$router.push({ name: "Details", params: { id: id } });
     },
     getAllTournaments() {
+      const currentDate = new Date().valueOf();
+
       db.collection("tournament")
         .get()
         .then(snapshot => {
           snapshot.forEach(doc => {
             const data = doc.data();
-            data.startDate = new Date(
-              data.startDate.seconds * 1000
-            ).toLocaleDateString("en-GB", {
-              year: "numeric",
-              month: "numeric",
-              day: "numeric",
-              hour12: false,
-              hour: "2-digit",
-              minute: "2-digit"
-            });
 
-            data.signupDate = new Date(
-              data.signupDate.seconds * 1000
-            ).toLocaleDateString("en-GB", {
-              year: "numeric",
-              month: "numeric",
-              day: "numeric",
-              hour12: false,
-              hour: "2-digit",
-              minute: "2-digit"
-            });
+            const date = new Date(data.startDate.seconds * 1000);
+            data.status = currentDate < date;
 
+            data.startDate = this.dateFormatter(date);
+
+            data.signupDate = this.dateFormatter(
+              new Date(data.signupDate.seconds * 1000)
+            );
             data.id = doc.id;
             this.tournaments.push(data);
-            console.log(data);
           });
         })
         .catch(err => {
           console.log("Error getting documents", err);
         });
+    },
+    dateFormatter(dt) {
+      return dt.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit"
+      });
     }
   }
 };
